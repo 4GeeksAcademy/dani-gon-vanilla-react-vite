a84483f1f7eed366e0f07bd10d4ec02c7510d5e7
Daniela González <danimge94@gmail.com>
2025-07-09 20:01:50 +0000

    imagenes ok

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -5,12 +5,14 @@ export const BasicCard = () => {
         <div>
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" 
-                src="https://fastly.picsum.photos/id/12/2500/1667.jpg?hmac=Pe3284luVre9ZqNzv1jMFpLihFI6lwq7TPgMSsNXw2w" />
+                src="https://fastly.picsum.photos/id/74/4288/2848.jpg?hmac=q02MzzHG23nkhJYRXR-_RgKTr6fpfwRgcXgE0EKvNB8" />
                 <Card.Body className="tarjeta">
-                    <Card.Title>Playas para ir este verano</Card.Title>
+                    <Card.Title>Opción de verano</Card.Title>
                     <Card.Text>
-                        Aquí te dejo una foto de una hermosa playa para que puedas 
-                        imaginarte con tu familia. Todas son válidas.
+                        Aquí te dejo una foto de un hermoso lago donde puedes hacer piragüismo, navegación a vela, 
+                        esquí acuático, wakeboarding, pesca, paddleboarding y snorkel,entre otros.
+                        Así podrás decidir más fácilmente, qué hacer con tu familia. 
+                        Todas las familias son válidas y puedes ir acompañadx de tu mascota.
                     </Card.Text>
                     <Button variant="primary">Press para ver más</Button>
                 </Card.Body>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,23 @@
-import { Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
 
 export const BarraNav = () => {
     return (
 
         <div>
             {[false].map((expand) => (
-                <Navbar key={expand} expand={expand} bg="dark" data-bs-theme="dark">
-                    <container className="Navegacion">
+                <Navbar  expand="lg" bg="dark" data-bs-theme="dark" key={expand}>
+                    <Container>
                         <Navbar.Brand href="#">Start Bootstrap</Navbar.Brand>
                         <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
                         <Navbar.Offcanvas
                             id={`offcanvasNavbar-expand-${expand}`}
                             aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
                             placement="end"
+                            className="desplegable"
                         >
                             <Offcanvas.Header closeButton />
                             <Offcanvas.Body>
-                                <Nav className="desplegable justify-content-end flex-grow-1 pe-3 bg-dark">
+                                <Nav className="opciones justify-content-end flex-grow-1 pe-3">
                                     <Nav.Link href="#home">Home</Nav.Link>
                                     <Nav.Link href="#about">About</Nav.Link>
                                     <Nav.Link href="#services">Services</Nav.Link>
@@ -24,7 +25,7 @@ export const BarraNav = () => {
                                 </Nav>
                             </Offcanvas.Body>
                         </Navbar.Offcanvas>
-                    </container>
+                    </Container>
                 </Navbar>
             ))}
         </div>
